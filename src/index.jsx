348ccab2627eb93c07b4061d12a86961ef611d97
pavel11123 <korobko.11@inbox.ru>
2023-05-27 22:24:47 +0600

    start project

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import HomePage from "./pages/HomePage/HomePage";
+import ShowPage from "./pages/ShowPage/ShowPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="/:id" element={<ShowPage />} />
+    </Routes>
+  </BrowserRouter>
+);
