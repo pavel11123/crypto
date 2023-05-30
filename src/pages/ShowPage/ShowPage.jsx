76e1962cd96e05190d3f7451ecb9a1c15f20d534
pavel11123 <korobko.11@inbox.ru>
2023-05-30 21:18:31 +0600

    add library recharts for graph crypto coin, and create the data display

--- src/pages/ShowPage/ShowPage.jsx
+++ src/pages/ShowPage/ShowPage.jsx
@@ -1,14 +1,46 @@
 import React from "react";
 import showStore from "../../stores/showStores";
+import { useParams } from "react-router-dom";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 export default function ShowPage() {
   // variable which contain request api
   const store = showStore();
+  // this hook need for get variabli from url
+  const params = useParams();
 
   // useEffect for update our component
   React.useEffect(() => {
-    store.fetchData();
+    store.fetchData(params.id);
   }, []);
 
-  return <div>show</div>;
+  return (
+    <div>
+      <AreaChart
+        width={500}
+        height={400}
+        data={store.graphData}
+        margin={{
+          top: 10,
+          right: 30,
+          left: 0,
+          bottom: 0,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="Date" />
+        <YAxis />
+        <Tooltip />
+        <Area type="monotone" dataKey="Price" stroke="#8884d8" fill="#8884d8" />
+      </AreaChart>
+    </div>
+  );
 }
--- src/stores/showStores.js
+++ src/stores/showStores.js
@@ -2,8 +2,25 @@ import axios from "axios";
 import { create } from "zustand";
 
 const showStore = create((set) => ({
-  fetchData: async () => {
-    console.log("pop");
+  graphData: [],
+
+  fetchData: async (id) => {
+    const response = await axios.get(
+      `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=365`
+    );
+    // console.log(response.data);
+
+    const graphData = response.data.prices.map((price) => {
+      const [timestamp, p] = price;
+      const date = new Date(timestamp).toLocaleDateString("en-us");
+
+      return {
+        Date: date,
+        Price: p,
+      };
+    });
+
+    set({ graphData }); // maybe save our elements which maps (graphData)
   },
 }));
 
