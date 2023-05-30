9796f2d61cd5d9e08aae1c6dd272f9698f6f2f77
pavel11123 <korobko.11@inbox.ru>
2023-05-30 21:53:21 +0600

    fix github pages

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import HomePage from "./pages/HomePage/HomePage";
 import ShowPage from "./pages/ShowPage/ShowPage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
+    {/* <BrowserRouter>  */}
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="/:id" element={<ShowPage />} />
     </Routes>
-  </BrowserRouter>
+    {/* </BrowserRouter> */}
+  </HashRouter>
 );
